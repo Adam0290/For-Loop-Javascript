c14df5f3991b3e52a8d9b6f53d2a70a30f8173b0
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:56:23 +0100

    Add files via upload

---
+++ For_loop/script.js
@@ -0,0 +1,7 @@
+// For Loop //
+
+for (let count = 0; count < arr.length ; count++) {  // initialization the counting Variable >>>>> let count = 0; //
+console.log(arr[count]);  // Decide the condition >>>>> count < arr.length //
+} // Range of a For Loop >>>>> Begins at the 0 and ends with the length >>>>>  0; count < arr.length
+
+// Formula of a For Loop >>>> For (initialization: condition; update ): //
